26603cada0bdcdd045cfaedb9f2095c8931076c4
DebashisDhali <dhalideba387@gmail.com>
2026-04-29 20:12:28 +0600

    Remove unused DonationForm import

--- client/src/components/PatientCard.js
+++ client/src/components/PatientCard.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import DonationForm from './DonationForm';
 import '../styles/PatientCard.css';
 
 const PatientCard = ({ patientId }) => {
